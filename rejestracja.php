9a6500d44ac2cc5280f1c50c2a7eb8967ba6b4c8
nookinka2121 <nookinka@gmail.com>
2015-08-06 19:19:12 +0200

    str_replace + walidacja pol formularza+ trim

--- rejestracja.php
+++ rejestracja.php
@@ -1,10 +1,21 @@
+
+<?php
+function formularz($login = "",$haslo1="",$haslo2="",$email="")
+{
+?> 
 <form method="POST" action="rejestracja.php">
-<b>Login:</b> <input type="text" name="login"><br><br>
-<b>Haslo:</b> <input type="password" name="haslo1"><br><br>
-<b>Powtorz haslo:</b> <input type="password" name="haslo2"><br><br>
-<b>Email:</b> <input type="text" name="email"><br><br>
+<b>Login:</b> <input type="text" name="login" value="<?= $login; ?>" /> <?php if( isset($_POST['rejestruj']) &&($login =="" || $login ==NULL)) echo "Podaj login (powyzej 4 znakow)"; ?><br><br />
+<b>Haslo:</b> <input type="password" name="haslo1"/><?php if( (isset($_POST['rejestruj']) && (($haslo1 =="" || $haslo1 ==NULL) || (($login =="" || $login ==NULL) || ($email =="" || $email ==NULL)) ) )) echo "Podaj haslo (powyzej 4 znakow)"; ?><br><br>
+<b>Powtorz haslo:</b> <input type="password" name="haslo2" /><?php if( isset($_POST['rejestruj']) &&($haslo2 =="" || $haslo2 ==NULL) && !($haslo1 =="" || $haslo1 ==NULL)) echo "Powtorz haslo"; ?><br><br>
+<b>Email:</b> <input type="text" name="email" value="<?= $email; ?>"/> <?php if( isset($_POST['rejestruj']) &&($email =="" || $email ==NULL)) echo "Podaj email"; ?><br><br />
 <input type="submit" value="Utworz konto" name="rejestruj">
 </form>
+
+
+
+<?php
+}
+?>
 
 <?php
 
@@ -16,40 +27,45 @@ include 'config.php';
 
 
 $sprawdz = '/([a-z|A-Z|0-9]{4,20})/';
-if ( !preg_match($sprawdz, $_POST["login"]) ) {
-    print 'Podaj poprawny login<br>';
-    $_POST["login"]=NULL;
+if ( !preg_match($sprawdz, str_replace(' ','',$_POST["login"]) )) {
+    //print 'Podaj poprawny login<br>';
+    $login=NULL;
 } else
 {
-$login = $_POST['login'];
+$login = str_replace(' ', '',$_POST['login']);
 }
  
  $sprawdz = '/([a-z|A-Z|0-9]{4,20})/';
-if ( !preg_match($sprawdz, $_POST["haslo1"]) ) {
-    print 'Podaj poprawne haslo<br>';
-    $_POST["haslo1"]=NULL;
+if ( !preg_match($sprawdz, str_replace(' ','',$_POST["haslo1"])) ){
+   // print 'Podaj poprawne haslo<br>';
+   $haslo1=NULL;
 } else
 {
-$haslo1= $_POST['haslo1'];
+$haslo1= str_replace(' ','',$_POST["haslo1"]);
 }
 
  
  $sprawdz = '/^([a-z|A-Z|0-9]{4,20})@([a-z|A-Z|0-9]{2,10})\\.(pl|gr|com)$/';
-if ( !preg_match($sprawdz, $_POST["email"]) ) {
-    print 'Podaj poprawny email<br>';
-    $_POST["email"]=NULL;
+if ( !preg_match($sprawdz,str_replace(' ','',$_POST["email"]) )) {
+   // print 'Podaj poprawny email<br>';
+    $email=NULL;
 } else
 {
-$email= $_POST['email'];
+$email= str_replace(' ','',$_POST["email"]);
 }
 
-$haslo2 = $_POST['haslo2'];
+$haslo2 = str_replace(' ','',$_POST["haslo2"]);
+if ($haslo1 != $haslo2) 
+{
+$haslo1=NULL;
+}
+
+formularz($login,$haslo1,$haslo2,$email);
 
 
   if (empty($_POST["haslo1"]) ||
         empty($_POST["haslo2"]) ||
         empty($_POST["login"]) || empty($_POST["email"])) {
-      print 'Podaj poprawne dane<br>';
     } 
 else{
 
@@ -64,16 +80,25 @@ if ($haslo1 == $haslo2)
 
 
 
-			echo "Konto zostalo utworzone!";
+			echo "Konto zostalo utworzone!<br>";
+            //nowy formularz.
+            //czyszczenie formul
+        
+
 		}
-		else echo "Hasla nie sa takie same";
+	
+
 
 }
 
 	
 }
 
+else
+{
 
+    formularz();
+}
 
 ?>
 <html><a href="index.php"><img src="wroc.png"/></a></html>
